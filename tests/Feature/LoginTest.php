76b1b7324414a134083f17a963bbc7c02b3066ca
Taras Maltsev <taras.maltsev@gmail.com>
2024-06-21 17:58:54 +0300

    login tests are created

---
+++ tests/Feature/LoginTest.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class LoginTest extends TestCase
+{
+    use RefreshDatabase, WithFaker;
+
+    protected $data;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->data = [
+            'email' => 'john.doe@example.com',
+            'password' => 'password123'
+        ];
+    }
+
+    public function test_login_successful()
+    {
+        User::factory()->create($this->data);
+        $this->assertDatabaseHas('users', [
+            'email' => 'john.doe@example.com',
+        ]);
+        $response = $this->postJson('/api/users/auth/login', $this->data);
+        $response->assertStatus(200);
+        $response->assertJsonStructure(['token']);
+        $responseData = $response->json();
+        $this->assertIsString($responseData['token']);
+        $this->assertNotEmpty($responseData['token']);
+    }
+
+    public function test_login_failure_if_email_is_not_verified()
+    {
+        User::factory()->create(array_merge($this->data, ['email_verified_at' => null]));
+        $this->assertDatabaseHas('users', [
+            'email' => 'john.doe@example.com',
+        ]);
+        $response = $this->postJson('/api/users/auth/login', $this->data);
+        $response->assertStatus(403);
+        $response->assertJson(['error' => 'Email is not verified.']);
+    }
+
+    public function test_login_failure_if_no_email_passed()
+    {
+        User::factory()->create($this->data);
+        $this->assertDatabaseHas('users', [
+            'email' => 'john.doe@example.com',
+        ]);
+        $response = $this->postJson('/api/users/auth/login', [
+            'password' => 'WrongPassword',
+        ]);
+        $response->assertStatus(422);
+        $response->assertJsonValidationErrors('email');
+    }
+
+    public function test_login_failure_if_wrong_password_passed()
+    {
+        User::factory()->create($this->data);
+        $this->assertDatabaseHas('users', [
+            'email' => 'john.doe@example.com',
+        ]);
+        $response = $this->postJson('/api/users/auth/login', [
+            'email' => 'john.doe@example.com',
+            'password' => 'WrongPassword',
+        ]);
+        $response->assertStatus(401);
+        $response->assertJson([
+            'error' => 'Email or password does not match the record.',
+        ]);
+    }
+
+    public function test_login_failure_if_week_password_passed()
+    {
+        User::factory()->create($this->data);
+        $this->assertDatabaseHas('users', [
+            'email' => 'john.doe@example.com',
+        ]);
+        $response = $this->postJson('/api/users/auth/login', [
+            'email' => 'john.doe@example.com',
+            'password' => '123',
+        ]);
+        $response->assertStatus(422);
+        $response->assertJson([
+            "message" => "The password field must be at least 8 characters.",
+        ]);
+    }
+
+    public function test_login_failure_if_user_does_not_exist()
+    {
+        $response = $this->postJson('/api/users/auth/login', $this->data);
+        $response->assertStatus(404);
+        $response->assertJson(['error' => 'User not found.']);
+    }
+}
