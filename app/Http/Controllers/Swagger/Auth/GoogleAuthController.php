befb197c7f80a18f218690af815c01e384f8e86e
Taras Maltsev <taras.maltsev@gmail.com>
2024-07-01 18:28:37 +0300

    docs for google auth routes are created

---
+++ app/Http/Controllers/Swagger/Auth/GoogleAuthController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers\Swagger\Auth;
+
+use App\Http\Controllers\Controller;
+
+/**
+ *  @OA\Get(
+ *     path="/auth/google/redirect",
+ *     tags={"Authentication"},
+ *     summary="Redirect to Google for authentication",
+ *     description="Redirects the user to Google for authentication",
+ *     @OA\Response(
+ *         response=302,
+ *         description="Redirect to Google"
+ *     ),
+ *     @OA\Response(
+ *         response=500,
+ *         description="Failed google redirect. Please try later."
+ *     )
+ *  ),
+ *  @OA\Get(
+ *     path="/auth/google/callback",
+ *     tags={"Authentication"},
+ *     summary="Handle Google callback",
+ *     description="Handles the callback from Google after authentication",
+ *     @OA\Response(
+ *         response=200,
+ *         description="Successfully handled Google callback"
+ *     ),
+ *     @OA\Response(
+ *         response=500,
+ *         description="Failed google callback. Please try later."
+ *     )
+ *  )
+ */
+
+class GoogleAuthController extends Controller
+{
+}
