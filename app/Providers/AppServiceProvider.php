58a79da6a2411ff3b16f6f717300b8161a076216
Taras Maltsev <taras.maltsev@gmail.com>
2024-07-11 12:47:27 +0300

    verification email letter is updated

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use App\Models\User;
 use Illuminate\Auth\Notifications\ResetPassword;
+use Illuminate\Auth\Notifications\VerifyEmail;
+use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -21,6 +23,14 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        VerifyEmail::toMailUsing(function (object $notifiable, string $url) {
+            return (new MailMessage)
+                ->subject('Verify Email Address')
+                ->greeting('Hello, ' . $notifiable->name . "!")
+                ->line('Please, click the button below to verify your email address.')
+                ->action('Verify Email Address', $url)
+                ->salutation('Best Regards, event-planner!');
+        });
         ResetPassword::createUrlUsing(function (User $user, string $token) {
             return 'http://127.0.0.1:8000/api/reset-password?token=' . $token;
         });
