eff55b03269d9b1c316527e3f7841144c5210142
Taras Maltsev <taras.maltsev@gmail.com>
2024-07-12 13:18:19 +0300

    docs view is fixed

--- app/Http/Controllers/Swagger/MainController.php
+++ app/Http/Controllers/Swagger/MainController.php
@@ -8,7 +8,7 @@ use App\Http\Controllers\Controller;
  *  @OA\Info(
  *      version="1.0.0",
  *      title="Event Planner API",
- *      description="Event Planner is a Laravel API designed to manage events. It allows users to create, read, update, and delete events. Additionally, the API provides authentication features, including email verification and Google sign-up.",
+ *      description="Event Planner API is a Laravel API designed to manage events. It allows users to create, read, update, and delete events. Additionally, the API provides authentication features, including email verification and Google sign-up.",
  *      contact={
  *          "name": "Taras Maltsev",
  *          "url": "https://github.com/TarasIT",
