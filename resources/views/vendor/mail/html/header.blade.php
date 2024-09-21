e52b63370b797ed8a20ce9652b676fae45caf398
Taras Maltsev <taras.maltsev@gmail.com>
2024-09-21 21:59:10 +0300

    logo for email letter is fixed

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -3,7 +3,7 @@
     <td class="header">
         <a href="{{ $url }}" style="display: inline-block;">
             @if (trim($slot) === 'Laravel')
-                <img src="{{ asset('event-planner-icon.png') }}" class="logo" alt="Event-planner Logo">
+                <img src="../../../../../public/event-planner-icon.png" class="logo" alt="Event-planner Logo">
             @else
                 {{ $slot }}
             @endif
