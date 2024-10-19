6d37e923764f2ade44e04b469058a9741fc98001
Taras Maltsev <taras.maltsev@gmail.com>
2024-10-19 21:03:09 +0300

    config method instead of env is used

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,7 +24,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(UrlGenerator $url): void
     {
-        if (env('APP_ENV') == 'production') {
+        if (config('app.env') == 'production') {
             $url->forceScheme('https');
         }
         VerifyEmail::toMailUsing(function (object $notifiable, string $url) {
