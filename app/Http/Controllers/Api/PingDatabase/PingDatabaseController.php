f8052e854559511a8735a276f8c1c4786ebc3e38
Taras Maltsev <taras.maltsev@gmail.com>
2024-10-19 20:09:19 +0300

    log errors and returning type are added

--- app/Http/Controllers/Api/PingDatabase/PingDatabaseController.php
+++ app/Http/Controllers/Api/PingDatabase/PingDatabaseController.php
@@ -3,16 +3,19 @@
 namespace App\Http\Controllers\Api\PingDatabase;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 
 class PingDatabaseController extends Controller
 {
-    public function ping()
+    public function ping(): JsonResponse
     {
         try {
             DB::connection()->getPdo();
             return response()->json(['status' => 'success', 'message' => 'Database connection is alive.'], 200);
-        } catch (\Exception $e) {
+        } catch (\Throwable $th) {
+            Log::error("Database connection failed: " . $th->getMessage());
             return response()->json(['status' => 'error', 'message' => 'Database connection failed.'], 500);
         }
     }
